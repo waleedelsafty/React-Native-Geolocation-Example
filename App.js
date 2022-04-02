ac9c8803f8f8dea47ba786ec0c2bc75b5fcda224
Waleed El-Safty <elsafty.waleed@gmail.com>
2022-04-03 00:59:18 +0200

    update for expo 44

--- App.js
+++ App.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+import * as Location from "expo-location";
 
 export default class App extends React.Component {
-  state= {
-    location:null,
-    geocode:null,
-    errorMessage:""
-  }
-  componentDidMount(){
-    this.getLocationAsync()
+  state = {
+    location: null,
+    geocode: null,
+    errorMessage: "",
+  };
+  componentDidMount() {
+    this.getLocationAsync();
   }
   getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    //let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
       this.setState({
-        errorMessage: 'Permission to access location was denied',
+        errorMessage: "Permission to access location was denied",
       });
     }
 
-    let location = await Location.getCurrentPositionAsync({accuracy:Location.Accuracy.BestForNavigation});
-    const { latitude , longitude } = location.coords
-    this.getGeocodeAsync({latitude, longitude})
-    this.setState({ location: {latitude, longitude}});
-
+    let location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.BestForNavigation,
+    });
+    const { latitude, longitude } = location.coords;
+    this.getGeocodeAsync({ latitude, longitude });
+    this.setState({ location: { latitude, longitude } });
   };
-  getGeocodeAsync= async (location) => {
-    let geocode = await Location.reverseGeocodeAsync(location)
-    this.setState({ geocode})
-  }
-  render(){
-    const {location,geocode, errorMessage } = this.state
+  getGeocodeAsync = async (location) => {
+    let geocode = await Location.reverseGeocodeAsync(location);
+    this.setState({ geocode });
+  };
+  render() {
+    const { location, geocode, errorMessage } = this.state;
     return (
-      <ImageBackground  source={require("./assets/bg.jpg")} blurRadius={5} style={styles.container}>
+      <ImageBackground
+        source={require("./assets/bg.jpg")}
+        blurRadius={5}
+        style={styles.container}
+      >
         <View style={styles.overlay}>
-          <Image source={require("./assets/marker.png")} style={{width:100,height:100}} />
-          <Text style={styles.heading1}>{geocode  ? `${geocode[0].city}, ${geocode[0].isoCountryCode}` :""}</Text>
-          <Text style={styles.heading2}>{geocode ? geocode[0].street :""}</Text>
-          <Text style={styles.heading3}>{location ? `${location.latitude}, ${location.longitude}` :""}</Text>
+          <Image
+            source={require("./assets/marker.png")}
+            style={{ width: 100, height: 100 }}
+          />
+          <Text style={styles.heading1}>
+            {geocode ? `${geocode[0].city}, ${geocode[0].isoCountryCode}` : ""}
+          </Text>
+          <Text style={styles.heading2}>
+            {geocode ? geocode[0].street : ""}
+          </Text>
+          <Text style={styles.heading3}>
+            {location ? `${location.latitude}, ${location.longitude}` : ""}
+          </Text>
           <Text style={styles.heading2}>{errorMessage}</Text>
-
         </View>
       </ImageBackground>
     );
@@ -50,32 +63,31 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  overlay:{
-    backgroundColor:"#00000070",
-    height:"100%",
-    width:"100%",
-    justifyContent:"center",
-    alignItems:"center"
+  overlay: {
+    backgroundColor: "#00000070",
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  heading1:{
-    color:"#fff",
-    fontWeight:"bold",
-    fontSize:30,
-    margin:20
+  heading1: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 30,
+    margin: 20,
   },
-  heading2:{
-    color:"#fff",
-    margin:5,
-    fontWeight:"bold",
-    fontSize:15
+  heading2: {
+    color: "#fff",
+    margin: 5,
+    fontWeight: "bold",
+    fontSize: 15,
   },
-  heading3:{
-    color:"#fff",
-    margin:5
-  }
+  heading3: {
+    color: "#fff",
+    margin: 5,
+  },
 });
